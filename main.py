da94e05b47bea606ab101ea9c5c3239275ff1475
1nv1sibl3 <ompritam2705@gmail.com>
2023-08-13 16:35:47 +0530

    MH/history

--- main.py
+++ main.py
@@ -23,13 +23,29 @@ def credits():
 def compare():
     return render_template("compare.html")
 
-@app.route("/ContactUs")
+@app.route("/feedback")
 def ContactUs():
-    return render_template("contact.html")
+    return render_template("feedback.html")
+
+
+# Error handling
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# MAHARASHTRA and its sub topics
+@app.route("/MH/etymology")
+def MH_etymology():
+    return render_template("MH/etymology.html")
+
+@app.route("/MH/history")
+def MH_history():
+    return render_template("MH/history.html")
+
 
 # Run webserver
 def run_webserver():
-    app.run(host='0.0.0.0', debug=True, port=8082)
+    app.run(host='0.0.0.0', debug=True, port=8080)
 
 if __name__ == "__main__":
     run_webserver()
